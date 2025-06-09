498b197db07a80499b1f081a4195b9743ab09bf7
jorditer <jordi.terradas6@gmail.com>
2025-06-09 23:44:58 +0200

    to-fail

---
+++ src/tests/auth.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, test } from "vitest";
+import { getAPIKey } from "src/api/auth";
+
+const person = {
+  isActive: true,
+  age: 32,
+};
+
+// describe("returns null", () => {
+//   test("If not request it returns null", () => {
+//     expect(getAPIKey(" ")).toBeDefined();
+//   });
+
+
+describe("person", () => {
+  test("person is defined", () => {
+    expect(person).toBeUndefined();
+  });
+
+  test("is active", () => {
+    expect(person.isActive).toBeTruthy();
+  });
+});
